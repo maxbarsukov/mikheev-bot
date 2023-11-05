fa4c3eaa5390816def3d88e5490b71f252d16582
maxbarsukov <maximbarsukov@bk.ru>
2023-11-05 19:19:33 +0300

    feat: :sparkles: add db wrapper

---
+++ lib/database.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'active_record'
+require 'logger'
+
+class Database
+  class << self
+    def establish_connection
+      ActiveRecord::Base.logger = Logger.new(active_record_logger_path)
+
+      configuration = YAML.safe_load(File.open(database_config_path))
+      ActiveRecord::Base.establish_connection(configuration)
+    end
+
+    private
+
+    def active_record_logger_path
+      'log/bot.log'
+    end
+
+    def database_config_path
+      'config/database.yml'
+    end
+  end
+end
